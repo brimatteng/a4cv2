0b7b26711e0468b4bf675688de9d60ef8edef870
brimatteng <btenggren20@gmail.com>
2014-11-04 17:21:12 -0500

    Add home page and what not!

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,2 +1,10 @@
 class FeaturesController <ApplicationController
+
+  def index
+    @features = Feature.all
+  end
+
+  def show
+    @feature = Feature.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,4 @@
 Rails.application.routes.draw do
+  resources :features, only: [:index, :show]
+  root 'home#show'
 end
